1f6ff3e1bfc28c8b4d3b5e5b419a10bac70372e6
Rony7736 <ronychakrabarty99@gmail.com>
2024-12-07 18:53:50 +0600

    added delete method in server site for delete data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config() 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -39,6 +39,13 @@ async function run() {
 
     app.get("/addcampaign", async(req, res) => {
       const result = await campaignCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.delete("/campaign/:id", async(req, res) => {
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await campaignCollection.deleteOne(query)
       res.send(result)
     })
 
